2c8afff0f096694a338e32b99be0f7d60d1e7131
nevadiesel <ivan@neva-diesel.com>
2024-03-14 16:29:40 +0300

    1.07

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from Ui_interface import Ui_MainWindow
 import numpy as np
 import sympy as sp
 import pyqtgraph as pg
+import pyqtgraph.exporters
 from PyQt5 import QtWidgets, QtCore, QtWidgets, QtSql, QtGui
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from PyQt5.QtGui import QIcon
-from PyQt5.QtSql import QSqlTableModel
+from PyQt5.QtSql import QSqlTableModel  
 
 
 class MainApplication(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -19,10 +20,11 @@ class MainApplication(QtWidgets.QMainWindow, Ui_MainWindow):
         self.directory = os.getcwd()
         self.setWindowTitle("PyCharts ")
         self.setWindowIcon(QIcon('pie-chart.ico'))
+        # self.setWindowIcon(QIcon('ico.ico'))
         self.table = None
         self.color = 'blue'
         self.marker = 'o'
-        self.lst_item_func = []
+        self.lst_item = []
         self.line_style = QtCore.Qt.SolidLine
         self.graphicsView.setBackground('#31394D')
         self.graphicsView.showGrid(x=True, y=True, alpha=1)
@@ -77,12 +79,28 @@ class MainApplication(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             newWidth = 0
 
-        self.animation = QtCore.QPropertyAnimation(self.right_menu_widget, b"maximumWidth")
+        self.animation = QtCore.QPropertyAnimation(
+            self.right_menu_widget, b"maximumWidth")
         self.animation.setDuration(250)
         self.animation.setStartValue(width)
         self.animation.setEndValue(newWidth)
         self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
         self.animation.start()
+
+        hight = self.tableView.height()
+
+        if hight == 0:
+            newhight = 192
+        else:
+            newhight = 0
+
+        self.animation1 = QtCore.QPropertyAnimation(
+            self.tableView, b"maximumHeight")
+        self.animation1.setDuration(250)
+        self.animation1.setStartValue(hight)
+        self.animation1.setEndValue(newhight)
+        self.animation1.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation1.start()
 
     def set_background_color(self, color):
         self.graphicsView.setBackground(color)
@@ -118,23 +136,24 @@ class MainApplication(QtWidgets.QMainWindow, Ui_MainWindow):
         # item = QTableVievItem(Func) # create the item
         # item.setTextAlignment(Qt.AlignHCenter)
 
-    def add_new_transaction(self):
-        function_text = self.select_funtion.text() if self.select_funtion.text() else 'xlsx'
+    def add_new_chart(self):
+        function_text = self.select_function.text(
+        ) if self.select_function.text() else 'xlsx'
         x_min = int(self.lineEdit_2.text())
         x_max = int(self.lineEdit.text())
         color = self.color
         line_style = self.line_style
 
-        self.add_new_transaction_query(
+        self.new_chart_query(
             function_text, x_min, x_max, color, line_style)
         self.view_data()
 
-    def add_new_transaction_query(self, function_text, x_min, x_max, color, line_style):
+    def new_chart_query(self, function_text, x_min, x_max, color, line_style):
         sql_query = "INSERT INTO charts (Func, Min, Max, Color, Style) VALUES (?, ?, ?, ?, ?)"
-        self.execute_query_with_params(
+        self.delete_query_id(
             sql_query, [function_text, x_min, x_max, color, line_style])
 
-    def execute_query_with_params(self, sql_query, query_values=None):
+    def delete_query_id(self, sql_query, query_values=None):
         query = QtSql.QSqlQuery()
         query.prepare(sql_query)
 
@@ -162,7 +181,7 @@ class MainApplication(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def delete_data_from_db(self, id):
         sql_query = "DELETE FROM charts WHERE ID=?"
-        self.execute_query_with_params(sql_query, [id])
+        self.delete_query_id(sql_query, [id])
 
     def delete_chart(self):
         index = self.tableView.selectedIndexes()[0]
@@ -170,50 +189,67 @@ class MainApplication(QtWidgets.QMainWindow, Ui_MainWindow):
         self.delete_data_from_db(id)
         self.view_data()
         id = int(id) - 1
-        self.graphicsView.removeItem(self.lst_item_func[id])
+        self.graphicsView.removeItem(self.lst_item[id])
+
+    def draw_fuctinon(self):
+        function_text = self.select_function.text()
+        x_min = int(self.lineEdit_2.text())
+        x_max = int(self.lineEdit.text())
+        x = np.linspace(x_min, x_max, int((x_max - x_min) / 0.01) + 1)
+        self.symbolSize = 0.1
+        try:
+            func = sp.lambdify(sp.Symbol('x'), function_text)
+            with np.errstate(divide='ignore', invalid='ignore'):
+                y = func(x)
+            # func = sp.lambdify(sp.Symbol('x'), function_text)
+            # def f(x):
+            #     with np.errstate(divide='ignore', invalid='ignore'):
+            #         return func(x)
+            # y = f(x)
+        except:
+            QMessageBox.warning(self, "Ошибка", "Ошибка в вычислении функции")
+
+        self.plot_item_func = self.graphicsView.plot(x, y, pen=pg.mkPen(
+            self.color, width=2, style=self.line_style), name=function_text)
+
+        self.lst_item.append(self.plot_item_func)
+
+    def draw_xlsx(self):
+        x = self.table.values[:, 0]
+        y = self.table.values[:, 1]
+        self.symbolSize = 15
+        self.plot_item_xlsx = self.graphicsView.plot(x, y, pen=pg.mkPen(
+            self.color, width=2, style=self.line_style), symbol=self.marker, symbolBrush='black', symbolSize=self.symbolSize, name='xlsx')
+
+        self.lst_item.append(self.plot_item_xlsx)
 
     def draw_chart(self):
-        function_text = self.select_funtion.text()
-        if function_text:
-            x = np.linspace(int(self.lineEdit.text()),
-                            int(self.lineEdit_2.text()), 1000)
-            self.symbolSize = 0.1
-            try:
-                func = sp.lambdify(sp.Symbol('x'), function_text)
-                y = func(x)
-            except:
-                QMessageBox.warning(
-                    self, "Ошибка", "Ошибка в вычислении функции")
-
-            self.plot_item_func = self.graphicsView.plot(x, y, pen=pg.mkPen(
-                self.color, width=2, style=self.line_style), name=function_text)
-
-            self.lst_item_func.append(self.plot_item_func)
+        if self.select_function.text():
+            self.draw_fuctinon()
+            self.save()
         else:
-            x = self.table.values[:, 0]
-            y = self.table.values[:, 1]
-            self.symbolSize = 15
-            self.plot_item_xlsx = self.graphicsView.plot(x, y, pen=pg.mkPen(
-                self.color, width=2, style=self.line_style), symbol=self.marker, symbolBrush='black', symbolSize=self.symbolSize, name='xlsx')
-
-            self.lst_item_func.append(self.plot_item_xlsx)
-
-        try:
-            print(self.lst_item_func)
-            print('________________')
-        except:
-            pass
-        self.add_new_transaction()
+            self.draw_xlsx()
+        self.add_new_chart()
 
     def clear_all(self):
         self.graphicsView.clear()
-        self.lst_item_func.clear()
+        self.lst_item.clear()
+        query = QtSql.QSqlQuery()
+        query.exec("DELETE FROM charts")
+        self.view_data()
 
     def before_close(self):
         query = QtSql.QSqlQuery()
         query.exec("DELETE FROM charts")
         os.unlink('chart_db.db')
         print("Данные удалены.")
+
+    def save(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getSaveFileName(self, "Save File", "", "SVG Files (*.svg)", options=options)
+        if fileName:
+            exporter = pg.exporters.SVGExporter(self.graphicsView.plotItem)
+            exporter.export(fileName+'.svg')
 
     def closeEvent(self, event):
         self.before_close()
